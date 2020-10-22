656f89485141c3ebb8cee17a805beac0a3b0e8ca
DDDDanny <707956456@qq.com>
2020-10-22 23:51:23 +0800

    Initial commit

---
+++ Common/Result.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/10/22 23:12
+# @Author  : DannyDong
+# @File    : Result.py
+# @describe: 封装处理请求结果
+
+from flask import jsonify
+
+
+class Result(object):
+    def __init__(self, **kwargs):
+        pass
+
+
